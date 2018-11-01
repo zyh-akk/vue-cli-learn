627131af2c2c4c2462f83482250b58363141f7bb
zyh <1832819123@qq..com>
2018-11-01 09:34:18 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "plugin:vue/essential",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    lintOnSave: undefined,
+    baseUrl: undefined,
+    outputDir: undefined,
+    assetsDir: 'staticAssets',
+    runtimeCompiler: undefined,
+    productionSourceMap: undefined,
+    parallel: undefined,
+    css: undefined
+}
